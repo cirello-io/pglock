d2b1c192c902254ff6c795f6553e198ee1ad5d06
Ulderico Cirello <u@cirello.eu>
2019-05-25 04:42:25 +0200

    pglock: improve code coverage

--- client_internal_test.go
+++ client_internal_test.go
@@ -17,12 +17,14 @@ limitations under the License.
 package pglock
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/lib/pq"
@@ -97,22 +99,177 @@ func (t *testLogger) Println(v ...interface{}) {
 }
 
 func TestDBErrorHandling(t *testing.T) {
-	t.Run("bad tx - acquire", func(t *testing.T) {
+	setup := func() (*Client, sqlmock.Sqlmock) {
 		db, err := sql.Open("postgres", "")
 		if err != nil {
 			t.Fatal("cannot connect to test database server:", err)
 		}
-		client, _ := New(db)
+		client, _ := New(db, WithHeartbeatFrequency(0))
 		db, mock, err := sqlmock.New()
 		if err != nil {
 			t.Fatal("cannot create mock:", err)
 		}
 		client.db = db
-		badTx := xerrors.New("transaction begin error")
-		mock.ExpectBegin().WillReturnError(badTx)
-		_, err = client.Acquire("bad-tx")
-		if !xerrors.Is(err, badTx) {
-			t.Errorf("expected tx missing: %v", err)
-		}
+		return client, mock
+	}
+	t.Run("acquire", func(t *testing.T) {
+		t.Run("bad tx", func(t *testing.T) {
+			client, mock := setup()
+			badTx := xerrors.New("transaction begin error")
+			mock.ExpectBegin().WillReturnError(badTx)
+			if _, err := client.Acquire("bad-tx"); !xerrors.Is(err, badTx) {
+				t.Errorf("expected tx error missing: %v", err)
+			}
+		})
+		t.Run("bad rvn", func(t *testing.T) {
+			client, mock := setup()
+			badRVN := xerrors.New("cannot load next RVN")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnError(badRVN)
+			if _, err := client.Acquire("bad-rvn"); !xerrors.Is(err, badRVN) {
+				t.Errorf("expected RVN error missing: %v", err)
+			}
+		})
+		t.Run("bad insert", func(t *testing.T) {
+			client, mock := setup()
+			badInsert := xerrors.New("cannot insert")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnRows(sqlmock.NewRows([]string{"nextval"}).AddRow(1))
+			mock.ExpectExec(`INSERT INTO locks (.+)`).WillReturnError(badInsert)
+			if _, err := client.Acquire("bad-insert"); !xerrors.Is(err, badInsert) {
+				t.Errorf("expected RVN error missing: %v", err)
+			}
+		})
+		t.Run("bad RVN confirmation", func(t *testing.T) {
+			client, mock := setup()
+			badRVN := xerrors.New("cannot confirm RVN")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnRows(sqlmock.NewRows([]string{"nextval"}).AddRow(1))
+			mock.ExpectExec(`INSERT INTO locks (.+)`).WillReturnResult(sqlmock.NewResult(0, 1))
+			mock.ExpectQuery(`SELECT "record_version_number", "data", "owner" FROM locks WHERE name = (.+)`).WillReturnError(badRVN)
+			if _, err := client.Acquire("bad-insert"); !xerrors.Is(err, badRVN) {
+				t.Errorf("expected RVN confirmation error missing: %v", err)
+			}
+		})
+		t.Run("bad commit", func(t *testing.T) {
+			client, mock := setup()
+			badCommit := xerrors.New("cannot confirm RVN")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnRows(sqlmock.NewRows([]string{"nextval"}).AddRow(1))
+			mock.ExpectExec(`INSERT INTO locks (.+)`).WillReturnResult(sqlmock.NewResult(0, 1))
+			mock.ExpectQuery(`SELECT "record_version_number", "data", "owner" FROM locks WHERE name = (.+)`).WillReturnRows(
+				sqlmock.NewRows([]string{
+					"record_version_number",
+					"data",
+					"owner",
+				}).AddRow(1, []byte{}, "owner"),
+			)
+			mock.ExpectCommit().WillReturnError(badCommit)
+			if _, err := client.Acquire("bad-insert"); !xerrors.Is(err, badCommit) {
+				t.Errorf("expected commit error missing: %v", err)
+			}
+		})
+	})
+	fakeLock := &Lock{
+		leaseDuration: time.Minute,
+	}
+	t.Run("release", func(t *testing.T) {
+		t.Run("bad tx", func(t *testing.T) {
+			client, mock := setup()
+			badTx := xerrors.New("transaction begin error")
+			mock.ExpectBegin().WillReturnError(badTx)
+			if err := client.Release(fakeLock); !xerrors.Is(err, badTx) {
+				t.Errorf("expected tx error missing: %v", err)
+			}
+		})
+		t.Run("bad update", func(t *testing.T) {
+			client, mock := setup()
+			badUpdate := xerrors.New("cannot update")
+			mock.ExpectBegin()
+			mock.ExpectExec(`UPDATE locks (.+)`).WillReturnError(badUpdate)
+			if err := client.Release(fakeLock); !xerrors.Is(err, badUpdate) {
+				t.Errorf("expected update error missing: %v", err)
+			}
+		})
+		t.Run("bad update result", func(t *testing.T) {
+			client, mock := setup()
+			badUpdateResult := xerrors.New("cannot grab update result")
+			mock.ExpectBegin()
+			mock.ExpectExec(`UPDATE locks (.+)`).WillReturnResult(sqlmock.NewErrorResult(badUpdateResult))
+			if err := client.Release(fakeLock); !xerrors.Is(err, badUpdateResult) {
+				t.Errorf("expected update result error missing: %v", err)
+			}
+		})
+		t.Run("bad delete", func(t *testing.T) {
+			client, mock := setup()
+			badDelete := xerrors.New("cannot delete lock entry")
+			mock.ExpectBegin()
+			mock.ExpectExec(`UPDATE locks (.+)`).WillReturnResult(sqlmock.NewResult(0, 1))
+			mock.ExpectExec(`DELETE FROM locks (.+)`).WillReturnError(badDelete)
+			if err := client.Release(fakeLock); !xerrors.Is(err, badDelete) {
+				t.Errorf("expected delete error missing: %v", err)
+			}
+		})
+		t.Run("bad commit", func(t *testing.T) {
+			client, mock := setup()
+			badCommit := xerrors.New("cannot commit release")
+			mock.ExpectBegin()
+			mock.ExpectExec(`UPDATE locks (.+)`).WillReturnResult(sqlmock.NewResult(0, 1))
+			mock.ExpectExec(`DELETE FROM locks (.+)`).WillReturnResult(sqlmock.NewResult(0, 1))
+			mock.ExpectCommit().WillReturnError(badCommit)
+			if err := client.Release(fakeLock); !xerrors.Is(err, badCommit) {
+				t.Errorf("expected commit error missing: %v", err)
+			}
+		})
+	})
+	t.Run("heartbeat", func(t *testing.T) {
+		t.Run("bad tx", func(t *testing.T) {
+			client, mock := setup()
+			badTx := xerrors.New("transaction begin error")
+			mock.ExpectBegin().WillReturnError(badTx)
+			if err := client.SendHeartbeat(context.Background(), fakeLock); !xerrors.Is(err, badTx) {
+				t.Errorf("expected tx error missing: %v", err)
+			}
+		})
+		t.Run("bad rvn", func(t *testing.T) {
+			client, mock := setup()
+			badRVN := xerrors.New("cannot load next RVN")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnError(badRVN)
+			if err := client.SendHeartbeat(context.Background(), fakeLock); !xerrors.Is(err, badRVN) {
+				t.Errorf("expected RVN error missing: %v", err)
+			}
+		})
+		t.Run("bad insert", func(t *testing.T) {
+			client, mock := setup()
+			badUpdate := xerrors.New("cannot insert")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnRows(sqlmock.NewRows([]string{"nextval"}).AddRow(1))
+			mock.ExpectExec(`UPDATE locks (.+)`).WillReturnError(badUpdate)
+			if err := client.SendHeartbeat(context.Background(), fakeLock); !xerrors.Is(err, badUpdate) {
+				t.Errorf("expected RVN error missing: %v", err)
+			}
+		})
+		t.Run("bad RVN confirmation", func(t *testing.T) {
+			client, mock := setup()
+			badRVN := xerrors.New("cannot confirm RVN")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnRows(sqlmock.NewRows([]string{"nextval"}).AddRow(1))
+			mock.ExpectExec(`UPDATE locks (.+)`).WillReturnResult(sqlmock.NewErrorResult(badRVN))
+			if err := client.SendHeartbeat(context.Background(), fakeLock); !xerrors.Is(err, badRVN) {
+				t.Errorf("expected RVN confirmation error missing: %v", err)
+			}
+		})
+		t.Run("bad commit", func(t *testing.T) {
+			client, mock := setup()
+			badCommit := xerrors.New("cannot confirm RVN")
+			mock.ExpectBegin()
+			mock.ExpectQuery(`SELECT nextval\('locks_rvn'\)`).WillReturnRows(sqlmock.NewRows([]string{"nextval"}).AddRow(1))
+			mock.ExpectExec(`UPDATE locks (.+)`).WillReturnResult(sqlmock.NewResult(0, 1))
+			mock.ExpectCommit().WillReturnError(badCommit)
+			if err := client.SendHeartbeat(context.Background(), fakeLock); !xerrors.Is(err, badCommit) {
+				t.Errorf("expected commit error missing: %v", err)
+			}
+		})
 	})
 }
